ee9d7f1e1b58e6202358861e98813a04dd184b88
Sujal Agrawal <sujalagrawal2700@gmail.com>
2025-11-03 14:44:08 +0530

    Implemented Onboarding Screen

--- app/src/main/java/com/sunflower/chaperone/MainActivity.kt
+++ app/src/main/java/com/sunflower/chaperone/MainActivity.kt
@@ -1,47 +1,85 @@
 package com.sunflower.chaperone
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.sunflower.chaperone.ui.navigation.auth.AppNavDisplay
+import com.sunflower.chaperone.ui.screens.onboarding.SplashScreen
 import com.sunflower.chaperone.ui.theme.ChaperoneTheme
+import com.sunflower.chaperone.ui.theme.whiteBG
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ChaperoneTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(whiteBG),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    AppNavDisplay(this@MainActivity)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ChaperoneTheme {
-        Greeting("Android")
+fun TwoPersonMap() {
+    val context = LocalContext.current
+
+    val mapStyleOptions = remember {
+        MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style_dark)
+    }
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(37.7749, -122.4194), 5f)
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        properties = MapProperties(mapStyleOptions = mapStyleOptions)
+    ) {
+        Marker(
+            state = MarkerState(position = LatLng(37.7749, -122.4194)),
+            title = "Person 1",
+            snippet = "Location: San Francisco"
+        )
+        Marker(
+            state = MarkerState(position = LatLng(34.0522, -118.2437)),
+            title = "Person 2",
+            snippet = "Location: Los Angeles"
+        )
     }
 }
--- app/src/main/java/com/sunflower/chaperone/ui/theme/Color.kt
+++ app/src/main/java/com/sunflower/chaperone/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.sunflower.chaperone.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple400 = Color(0xFF4F2B5A)
+val LightYellow = Color(0xFFFFFFF0)
+val BGPurple = Color(0xFFFFFBF8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val whiteBG = Color(0xFFFFFBF8)
+val whiteUBG = Color(0xFFF7F7F7)
+val progressBarPurple = Color(0xFF6750A4)
+val buttonPurple = Color(0xFF4B0962)
+val textBold = Color(0xFF171717)
+val textGray = Color(0xFF737373)
+val textPurple = Color(0xFF4B0962)
+val textDefault = Color(0xFF1E1E1E)
+val lightPurple = Color(0xFFE8D8F5)
+val mediumPurple = Color(0xFF9262B7)
+val lightGray = Color.LightGray
